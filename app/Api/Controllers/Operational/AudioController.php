8ba13f20a5d1cfa5c5c5629ac96c05a86be89df7
wangxiao <wangxiao@guadoutech.com>
2021-12-22 15:00:20 +0800

    修改列宽

---
+++ app/Api/Controllers/Operational/AudioController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Api\Controllers\Operational;
+
+use App\Api\Services\Operational\AudioService;
+use App\Http\Controllers\Controller;
+use App\Api\Requests\Admin\Unit\IndexRequest;
+
+class AudioController extends Controller
+{
+    public AudioService $service;
+
+    public function __construct()
+    {
+        $this->service = new AudioService();
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/index.php/api/operational/audio",
+     *     tags={"Operational/Unit"},
+     *     summary="音频列表",
+     *     operationId="audio",
+     *     security={ { "bearerAuth":{}}},
+     *     @OA\Parameter(in="query",name="page",description="当前页",schema={"type":"integer"},required=false),
+     *     @OA\Parameter(in="query",name="limit",description="每页条数",schema={"type":"integer"},required=false),
+     *     @OA\Response(response=200, description="success",@OA\JsonContent()),
+     * )
+     */
+    public function index(IndexRequest $request)
+    {
+        return apiReturn($this->service->index($request));
+    }
+}
---
+++ app/Api/Services/Operational/AudioService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Api\Services\Operational;
+
+use App\Models\Audio;
+use Illuminate\Http\Request;
+//use App\Api\Transformers\Operational\Audio\IndexTransformer;
+
+class AudioService
+{
+    public Audio $model;
+
+    public function __construct()
+    {
+        $this->model = new Audio();
+    }
+
+    /**
+     * @param  Request  $request
+     *
+     * @return array
+     */
+    public function index(Request $request)
+    {
+        return $this->model->get();
+    }
+
+}
