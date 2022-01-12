944d114079336808cdd9fec67f4b7e61f023861f
wangxiao <wangxiao@guadoutech.com>
2022-01-12 14:54:16 +0800

    scenarios相关表

--- database/migrations/2022_01_12_065354_create_scenario_light_table.php
+++ database/migrations/2022_01_12_065354_create_scenario_light_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateScenariosTable extends Migration
+class CreateScenarioLightTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateScenariosTable extends Migration
      */
     public function up()
     {
-        Schema::create('scenarios', function (Blueprint $table) {
+        Schema::create('scenario_light', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->unsignedTinyInteger('type')->comment('培训类型[1：Group,2：Individual]');
-            $table->unsignedInteger('rts_script_id')->comment('关联rts_scripts表id');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateScenariosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('scenarios');
+        Schema::dropIfExists('scenario_light');
     }
 }
