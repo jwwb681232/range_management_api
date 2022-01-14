82f1986a3109795560666d2ded619de486922652
wangxiao <wangxiao@guadoutech.com>
2022-01-14 10:52:59 +0800

    add deleted_at to table

--- app/Models/Audio.php
+++ app/Models/Audio.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Audio extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'audios';
     protected $primaryKey = 'id';
     protected $guarded = ['_token'];
