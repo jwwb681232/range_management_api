ce7cb5adffdef839a7ab182a50350d440d3dcfff
wangxiao <wangxiao@guadoutech.com>
2022-01-17 13:19:06 +0800

    add light tables

---
+++ database/migrations/2022_01_17_114308_create_lights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lights', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('number');
+            $table->string('name');
+            $table->unsignedBigInteger('deck_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lights');
+    }
+}
