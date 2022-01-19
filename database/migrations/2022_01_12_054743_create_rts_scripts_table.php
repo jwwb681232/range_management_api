085e2b71998e06b476c7c23602a817c8839c5332
wangxiao <wangxiao@guadoutech.com>
2022-01-19 17:27:37 +0800

    rts_scripts index field

--- database/migrations/2022_01_12_054743_create_rts_scripts_table.php
+++ database/migrations/2022_01_12_054743_create_rts_scripts_table.php
@@ -16,7 +16,7 @@ class CreateRtsScriptsTable extends Migration
         Schema::create('rts_scripts', function (Blueprint $table) {
             $table->id();
             $table->string('range_name');
-            $table->unsignedInteger('index')->comment('脚本index');
+            $table->string('index',32)->comment('脚本index');
             $table->string('name')->comment('脚本名称');
             $table->unsignedInteger('scenario_id')->comment('场景id');
             $table->string('scenario_name')->comment('场景名称');
