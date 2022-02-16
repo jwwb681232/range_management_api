964502f9e3b1fd287e3151bb3b3300d25a88f354
wangxiao <wangxiao@guadoutech.com>
2022-02-16 09:51:31 +0800

    运行使用websocket刷卡登陆

--- app/Api/Requests/Auth/LoginWithCardRequest.php
+++ app/Api/Requests/Auth/LoginWithCardRequest.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
 class LoginWithCardRequest extends FormRequest
@@ -30,7 +31,7 @@ class LoginWithCardRequest extends FormRequest
     {
         return [
             'id'          => 'required|integer|exists:users,id',
-            'card_number' => 'required|integer|exists:cards,number',
+            'card_number' => 'required',
         ];
     }
 
@@ -41,14 +42,27 @@ class LoginWithCardRequest extends FormRequest
     {
         $validator->after(function ($validator) {
 
+            $id         = $this->request->get('id');
+            $cardNumber = $this->request->get('card_number');
+
             // 用户与卡号不匹配
-            $user = User::with('card')->find($this->request->get('id'));
-            if ($user->card->number != $this->request->get('card_number')) {
-                $validator->errors()->add('card_number', 'This card does not belong to the user');
+            $user = User::with('card')->find($id);
+
+            // 刷卡登陆 Or 输入卡号登陆
+            if (Str::length($cardNumber) == 0){
+                $validator->errors()->add('card_number', "Illegal login");
+            }elseif(Str::length($cardNumber) == 1){
+                if ($cardNumber == '0'){
+                    $validator->errors()->add('card_number', "you don't have permission to login");
+                }
+            }else{
+                if ($user->card->number != $cardNumber) {
+                    $validator->errors()->add('card_number', 'This card does not belong to the user');
+                }
             }
 
             // 没有临时Token
-            $tempToken = Cache::get(CacheKeys::tempToken($this->request->get('id')));
+            $tempToken = Cache::get(CacheKeys::tempToken($id));
             if (!$tempToken){
                 $validator->errors()->add('id', 'Verification timed out, please login again');
             }
