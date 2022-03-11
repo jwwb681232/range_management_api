1c927d62eb1b127c614ce13824a10e04fb8c626e
wangxiao <wangxiao@guadoutech.com>
2022-03-11 10:27:28 +0800

    update

--- app/Api/Services/Operational/DoorService.php
+++ app/Api/Services/Operational/DoorService.php
@@ -23,7 +23,17 @@ class DoorService
     {
         $doors = [];
         foreach ($request->all() as $item) {
-            $doors[] = $this->model->updateOrCreate(['door_id'=>$item['Id']],['code'=>$item['Code'],'description'=>$item['Description'],'reader_no'=>$item['ReaderNo'],'controller_id'=>$item['ControllerId']]);
+            $doors[] = $this->model->updateOrCreate(
+                [
+                    'door_id' => $item['Id']
+                ],
+                [
+                    'code'          => $item['Code'],
+                    'description'   => $item['Description'],
+                    'reader_no'     => $item['ReaderNo'],
+                    'controller_id' => $item['ControllerId'],
+                ]
+            );
         }
 
         return $doors;
